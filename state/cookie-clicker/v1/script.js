2ad5f35d24c3558c661ffe09ac87a827a1636c09
Eddie <eddielin978@gmail.com>
2026-06-30 20:07:29 -0400

    Cookie clicker: Cookie + cookie count

--- state/cookie-clicker/v1/script.js
+++ state/cookie-clicker/v1/script.js
@@ -1,5 +1,15 @@
 const perClickUpgradeBtn = document.querySelector(".cpc-upgrade-btn");
 const perSecUpgradeBtn = document.querySelector(".cps-upgrade-btn");
-const cookie = document.querySelector("cookie");
+const cookie = document.querySelector(".cookie");
+const countText = document.querySelector(".cookie-count");
 let perClick = 1;
 let perSec = 0;
+let cookies = 0;
+function updateCount() {
+  countText.textContent = `Cookies: ${cookies}`;
+}
+updateCount();
+cookie.addEventListener("click", () => {
+  cookies++;
+  updateCount();
+});
