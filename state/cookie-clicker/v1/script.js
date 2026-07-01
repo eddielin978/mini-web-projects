ed9e53b7ac254b322ed846d0a13a7347be81d055
Eddie <eddielin978@gmail.com>
2026-07-01 16:17:09 -0400

    Cookie clicker: Cookies per second functionality
    Used setInterval() to allow perSec values to increment cookies
    Accounted for natural setInterval() rate-limiting
    Currently, the interval is fixed, so changes in perSec after declaration don't work

--- state/cookie-clicker/v1/script.js
+++ state/cookie-clicker/v1/script.js
@@ -3,13 +3,22 @@ const perSecUpgradeBtn = document.querySelector(".cps-upgrade-btn");
 const cookie = document.querySelector(".cookie");
 const countText = document.querySelector(".cookie-count");
 let perClick = 1;
-let perSec = 0;
+let perSec = 2001;
 let cookies = 0;
+let playing = true;
+let rateLimit = 1000 / perSec <= 4 ? true : false;
 function updateCount() {
-  countText.textContent = `Cookies: ${cookies}`;
+  countText.textContent = `Cookies: ${cookies.toFixed(0)}`;
 }
+const cpsInterval = setInterval(
+  () => {
+    rateLimit ? (cookies += perSec / 250) : (cookies += 1);
+    updateCount();
+  },
+  rateLimit ? 4 : 1000 / perSec,
+);
 updateCount();
 cookie.addEventListener("click", () => {
-  cookies++;
+  cookies += perClick;
   updateCount();
 });
