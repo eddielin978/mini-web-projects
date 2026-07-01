effc28e9b2952b019630f77ae84c54ca5f741ab2
Eddie <eddielin978@gmail.com>
2026-07-01 17:52:08 -0400

    Cookie clicker: Building upgrade buttons (not fully functional)
    Added building names and upgrade buttons
    Scaling cost
    Amount doesn't work yet
    Scaling perClick, but perSec doesn't work yet
    Costs don't check or detract cookies yet

--- state/cookie-clicker/v1/script.js
+++ state/cookie-clicker/v1/script.js
@@ -5,12 +5,21 @@ const perClickUpgradeBtn = document.querySelector(".cpc-upgrade-btn");
 const perSecUpgradeBtn = document.querySelector(".cps-upgrade-btn");
 const cookie = document.querySelector(".cookie");
 const countText = document.querySelector(".cookie-count");
+const cpcBuildingCountText = document.querySelector(".cpc-building .building-count");
+const cpsBuildingCountText = document.querySelector(".cps-building .building-count");
+const cpcBuildingBtn = document.querySelector(".cpc-building-btn");
+const cpsBuildingBtn = document.querySelector(".cps-building-btn");
 
 // Variable declaration
 let perClick = 1; // Cookies per click (eventually upgradable; starts at 1)
-let perSec = 30200; // Cookies per second (eventually upgradable; starts at 0)
+let perSec = 0; // Cookies per second (eventually upgradable; starts at 0)
 let cookies = 0; // Amount of cookies (increased by perClick and perSec; starts at 0)
-let rateLimit = 1000 / perSec <= 4 ? true : false; // setInterval() has a rate-limit of 4ms (250 iterations per second)
+let rateLimit = perSec === 0 ? false : 1000 / perSec <= 4 ? true : false; // setInterval() has a rate-limit of 4ms (250 iterations per second)
+let cpcBuildings = 0;
+let cpsBuildings = 0;
+let initialCost = 10;
+let cpcBuildingCost = initialCost;
+let cpsBuildingCost = initialCost;
 
 // Updates the span that shows how many cookies the user has
 function updateCount() {
@@ -33,3 +42,41 @@ const cpsInterval = setInterval(
   },
   rateLimit ? 4 : 1000 / perSec,
 );
+if (perSec === 0) clearInterval(cpsInterval);
+
+function updateBuildingCount() {
+  cpcBuildingCountText.textContent = `Amount: ${cpcBuildings}`;
+  cpsBuildingCountText.textContent = `Amount: ${cpsBuildings}`;
+}
+updateBuildingCount();
+
+function updateCostDisplay() {
+  cpcBuildingBtn.textContent = `Cost: ${cpcBuildingCost} cookies`;
+  cpsBuildingBtn.textContent = `Cost: ${cpsBuildingCost} cookies`;
+}
+updateCostDisplay();
+
+function updateCost(buildingType = "cpc" || "cps") {
+  if (buildingType === "cpc") {
+    cpcBuildingCost *= 4;
+  } else {
+    cpsBuildingCost *= 4;
+  }
+  updateCostDisplay();
+}
+
+function updateStat(buildingType = "cpc" || "cps") {
+  if (buildingType === "cpc") {
+    perClick += 3 + Math.floor(cpcBuildingCost / 40);
+  } else {
+    perSec++;
+  }
+  updateCost(buildingType);
+}
+
+cpcBuildingBtn.addEventListener("click", () => {
+  updateStat("cpc");
+});
+cpsBuildingBtn.addEventListener("click", () => {
+  updateStat("cps");
+});
