07078865d56c6eb98a01814ed5acb18addb7a6be
Eddie <eddielin978@gmail.com>
2026-07-04 13:41:58 -0400

    Text-calculator: Logic and invalid input handling
    Using the promptSync import to get user input
    Using typescript to evaluate the inputs
    Returning 'Please answer correctly!' for non-number num1/num2 inputs and non-symbol operation inputs

--- text-based/text-calculator/calculator.ts
+++ text-based/text-calculator/calculator.ts
@@ -1,2 +1,23 @@
 import promptSync from "prompt-sync";
 const prompt = promptSync({ sigint: true });
+let num1: number = parseInt(prompt("Enter number 1: "));
+let operation: string = prompt("Enter operation (+, -, *, or /): ");
+let num2: number = parseInt(prompt("Enter number 2: "));
+function doMath(num1: number, num2: number, operation: string) {
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    return "Please answer correctly!";
+  }
+  switch (operation) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    default:
+      return "Please answer correctly!";
+  }
+}
+console.log(`Answer: ${doMath(num1, num2, operation)}`);
