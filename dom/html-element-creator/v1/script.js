eec0b9e7053d854ff9582b4b7119027ba59b58c7
Eddie <eddielin978@gmail.com>
2026-07-13 08:51:51 -0400

    HTML Element Creator: Structure

---
+++ dom/html-element-creator/v1/script.js
@@ -0,0 +1,21 @@
+const listTemplate = document.querySelector(".list-template");
+const space = document.querySelector(".space");
+const buttons = document.querySelectorAll(".button");
+const createFuncArray = [
+  createH1,
+  createH2,
+  createH3,
+  createP,
+  createBtn,
+  createSquare,
+  createCircle,
+  createList,
+];
+function createH1() {}
+function createH2() {}
+function createH3() {}
+function createP() {}
+function createBtn() {}
+function createSquare() {}
+function createCircle() {}
+function createList() {}
