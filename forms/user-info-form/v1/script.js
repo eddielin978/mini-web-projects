8f983e48880f75d0247c0b4cff03fa30d0d5bc57
Eddie <eddielin978@gmail.com>
2026-07-20 12:41:17 -0400

    User Info Form: User-info list display functionality

--- forms/user-info-form/v1/script.js
+++ forms/user-info-form/v1/script.js
@@ -4,6 +4,29 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
   const formEntries = Object.fromEntries(formData.entries());
-  userList.textContent = formEntries.name;
+  addUserToList(formEntries, userList);
   e.target.reset();
 });
+function addUserToList(entries, list) {
+  let fragment = new DocumentFragment();
+  const nameSpan = document.createElement("span");
+  const emailSpan = document.createElement("span");
+  const phoneSpan = document.createElement("span");
+  nameSpan.textContent = `Name: ${entries?.name}`;
+  emailSpan.textContent = `Email: ${entries?.email}`;
+  phoneSpan.textContent = `Phone number: ${entries?.["phone-number"]}`;
+  if (nameSpan.textContent !== "Name: ") {
+    nameSpan.style.display = "block";
+    fragment.append(nameSpan);
+  }
+  if (emailSpan.textContent !== "Email: ") {
+    emailSpan.style.display = "block";
+    fragment.append(emailSpan);
+  }
+  if (phoneSpan.textContent !== "Phone number: ") {
+    phoneSpan.style.display = "block";
+    fragment.append(phoneSpan);
+  }
+  list.append(document.createElement("li"));
+  list.lastElementChild.append(fragment);
+}
