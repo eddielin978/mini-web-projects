857f7ba9e8b47549852ac9bb6b51f8ee5959a2c7
Eddie <eddielin978@gmail.com>
2026-08-01 10:18:03 -0400

    Prism JS: Creating more CSS files
    Created base.css, components.css, and layout.css, as well as a back button to go from pages/html.html to index.html

--- html-css/prism-js/v1/scripts/global-styles.js
+++ html-css/prism-js/v1/scripts/global-styles.js
@@ -1,4 +1,7 @@
 import "../styles/reset.css";
+import "../styles/base.css";
+import "../styles/layout.css";
+import "../styles/components.css";
 import "../styles/theme.css";
 import Prism from "prismjs";
 import "../styles/prism-overrides.css";
