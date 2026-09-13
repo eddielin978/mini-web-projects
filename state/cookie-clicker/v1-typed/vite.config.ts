3c0d65208a81a0b0b40182a648779c68d26ae830
Eddie <eddielin978@gmail.com>
2026-09-13 09:18:23 -0400

    Editing YAML for Cookie Clicker V1-typed

--- state/cookie-clicker/v1-typed/vite.config.ts
+++ state/cookie-clicker/v1-typed/vite.config.ts
@@ -4,11 +4,9 @@ import checker from "vite-plugin-checker";
 export default defineConfig({
   plugins: [
     checker({
-      // Enable TypeScript checking
       typescript: true,
-      // Enable ESLint checking
       eslint: {
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        lintCommand: "eslint .",
       },
     }),
   ],
